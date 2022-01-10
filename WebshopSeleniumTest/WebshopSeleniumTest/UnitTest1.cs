6ae8a0d5b75be111e7e6a95bf520cc41fbe103b2
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-10 14:02:54 +0100

    A test method to test the webdriver

--- WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
+++ WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
@@ -1,17 +1,26 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace WebshopSeleniumTest
 {
     public class Tests
     {
+        private IWebDriver Driver;
+
         [SetUp]
         public void Setup()
         {
+            // Hozzk létre a szükséges driver-t, itt adjuk meg, h milyen böngészõt akarunk használni
+            Driver = new ChromeDriver();
         }
 
         [Test]
         public void Test1()
         {
+            //Adjuk meg a website címét, amit meg akarunk nyitni a kiválasztott böngészõvel
+            Driver.Navigate().GoToUrl("https://www.gamestar.hu/");
+
             Assert.Pass();
         }
     }
