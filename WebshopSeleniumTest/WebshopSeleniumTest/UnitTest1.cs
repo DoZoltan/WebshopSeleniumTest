5ebc710b89d40a90ed39a6304c4a52c918f037d4
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-11 10:31:41 +0100

    Select dropdown element

--- WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
+++ WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
@@ -18,14 +18,16 @@ namespace WebshopSeleniumTest
         public void Test1()
         {
             Driver.Navigate().GoToUrl("http://localhost:4200/products/cpu");
+            Driver.Manage().Window.FullScreen();
 
-            // Ha nincs azonosítója az elemönknek / tag-ünk nek akkor az elérési útvonalát kell megadni
-            // --> a nav-link text-white class névvel elátott div tag-en belüli label, aminek az innertext-je Brand
-            // a Click()-el pedig rákattintunk a kiválasztott elemre
-            Driver.FindElement(By.XPath("//div[@class='nav-link text-white']//label[text()='Brand']")).Click();
+            // Válasszuk ki a fejléc menübõl az Admin lehetõséget, majd kattintsunk rá
+            Driver.FindElement(By.XPath("//a[@href='/admin']")).Click();
 
-            // majd írd be ezt az inputba
-            Driver.FindElement(By.ClassName("search-input")).SendKeys("Asus");
+            //A bal menüsávból az Add new product lehetõséget
+            Driver.FindElement(By.XPath("//a[@href='/admin/products/add']")).Click();
+
+            // Majd válasszuk ki a dropdown input-ot ($x("//select[@id='socketType']"))
+            Driver.FindElement(By.XPath("//select[@id='socketType']//option[text()='G34']")).Click();
 
             Assert.Pass();
         }
