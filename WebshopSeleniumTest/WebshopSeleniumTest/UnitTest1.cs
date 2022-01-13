76a1464477982a8e52fe0138f1aae73134c51518
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-13 11:01:43 +0100

    Handle new tabs

--- WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
+++ WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
@@ -20,35 +20,44 @@ namespace WebshopSeleniumTest
         }
 
         [Test]
-        public void TestDropdownAtCreateProductMenu()
+        public void PracticeTest()
         {
             Driver.Navigate().GoToUrl("http://localhost:4200/products/cpu");
 
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
 
-            //Válazd ki az elemet, amire gomb kombinációval akarsz kattintani
             var adminButton = Driver.FindElement(By.LinkText("Admin"));
 
-            //Hozz létre egy actions-öt
             Actions actions = new Actions(Driver);
 
-            //Add meg a gomb kombinációkat és a kiválasztott elemet
-            actions.KeyDown(Keys.LeftControl).Click(adminButton);
-            
-            //Építsd fel a kombinációt
+            actions.KeyDown(Keys.LeftControl).Click(adminButton).KeyUp(Keys.LeftControl);
+
             IAction multiple = actions.Build();
-            
-            //Hajtsd végre a mûveletet
+
             multiple.Perform();
 
-            //Nyisd meg az admin felületet
-            //Driver.FindElement(By.LinkText("Admin"));
+            string newTabUrl = "http://localhost:4200/admin";
 
-            //Nyisd meg a termék frissítés felületet
-            //Driver.FindElement(By.XPath("//a[@href='/admin/products/update']")).Click();
+            bool isSuccessful = true;
 
-            //A termék frissítésnél üsd be a keresõ mezõbe, hogy ram
-            //Driver.FindElement(By.ClassName("search-input")).SendKeys("ram");
+            if (Driver.WindowHandles.Count != 2)
+            {
+                isSuccessful = false;
+            }
+
+            if (isSuccessful)
+            {
+                Driver.SwitchTo().Window(Driver.WindowHandles[1]);
+
+                //Nyisd meg az admin felületet
+                //Driver.FindElement(By.LinkText("Admin")).Click();
+
+                //Nyisd meg a termék frissítés felületet
+                Driver.FindElement(By.XPath("//a[@href='/admin/products/update']")).Click();
+
+                //A termék frissítésnél üsd be a keresõ mezõbe, hogy ram
+                Driver.FindElement(By.ClassName("search-input")).SendKeys("ram");
+            }
 
             //Az elsõ találatra kattints rá a grid-en
             //Driver.FindElement(By.XPath("//div[@class='ag-row-even ag-row-no-focus ag-row ag-row-level-0 ag-row-position-absolute ag-row-first ag-after-created']/div[1]")).Click();
