d805ac65eda11a71880017d8d48710a3cf0c8d63
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-13 08:49:50 +0100

    Using key combinations to open a new tab

--- WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
+++ WebshopSeleniumTest/WebshopSeleniumTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
@@ -25,31 +26,38 @@ namespace WebshopSeleniumTest
 
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
 
-            //WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            //Válazd ki az elemet, amire gomb kombinációval akarsz kattintani
+            var adminButton = Driver.FindElement(By.LinkText("Admin"));
 
-            //Driver.FindElement(By.XPath("//div[@class='product-container']//ul/li[3]")).Click();
+            //Hozz létre egy actions-öt
+            Actions actions = new Actions(Driver);
 
-            //var gridElement = wait.Until(ExpectedConditions.ElementExists(By.XPath("//div[@class='grid-container']//div[@class='ag-row-odd ag-row-no-focus ag-row ag-row-level-0 ag-row-position-absolute ag-row-last ag-after-created'][1]/div[2]")));
-
-            //gridElement.Click();
+            //Add meg a gomb kombinációkat és a kiválasztott elemet
+            actions.KeyDown(Keys.LeftControl).Click(adminButton);
+            
+            //Építsd fel a kombinációt
+            IAction multiple = actions.Build();
+            
+            //Hajtsd végre a mûveletet
+            multiple.Perform();
 
             //Nyisd meg az admin felületet
-            Driver.FindElement(By.LinkText("Admin")).Click();
+            //Driver.FindElement(By.LinkText("Admin"));
 
             //Nyisd meg a termék frissítés felületet
-            Driver.FindElement(By.XPath("//a[@href='/admin/products/update']")).Click();
+            //Driver.FindElement(By.XPath("//a[@href='/admin/products/update']")).Click();
 
             //A termék frissítésnél üsd be a keresõ mezõbe, hogy ram
-            Driver.FindElement(By.ClassName("search-input")).SendKeys("ram");
+            //Driver.FindElement(By.ClassName("search-input")).SendKeys("ram");
 
             //Az elsõ találatra kattints rá a grid-en
-            Driver.FindElement(By.XPath("//div[@class='ag-row-even ag-row-no-focus ag-row ag-row-level-0 ag-row-position-absolute ag-row-first ag-after-created']/div[1]")).Click();
+            //Driver.FindElement(By.XPath("//div[@class='ag-row-even ag-row-no-focus ag-row ag-row-level-0 ag-row-position-absolute ag-row-first ag-after-created']/div[1]")).Click();
 
             //Kattints rá a törlés gombra
-            Driver.FindElement(By.XPath("//button[text()='Delete']")).Click();
+            //Driver.FindElement(By.XPath("//button[text()='Delete']")).Click();
 
             //Válts át a felugró megerõsítõ ablakra és fogadd el
-            Driver.SwitchTo().Alert().Accept();
+            //Driver.SwitchTo().Alert().Accept();
 
             Assert.Pass();
         }
