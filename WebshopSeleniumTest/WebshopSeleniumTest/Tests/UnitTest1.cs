3de4c18b9be2c91888f2749877391129da5647f1
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2022-01-13 14:11:01 +0100

    Test by POM

--- WebshopSeleniumTest/WebshopSeleniumTest/Tests/UnitTest1.cs
+++ WebshopSeleniumTest/WebshopSeleniumTest/Tests/UnitTest1.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using System;
+using WebshopSeleniumTest.POMs;
 
 namespace Tesets.WebshopSeleniumTest
 {
@@ -22,44 +23,22 @@ namespace Tesets.WebshopSeleniumTest
         [Test]
         public void PracticeTest()
         {
-            Driver.Navigate().GoToUrl("http://localhost:4200/products/cpu");
-
+            //Driver.Navigate().GoToUrl("http://localhost:4200/products/cpu");
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
 
-            var adminButton = Driver.FindElement(By.LinkText("Admin"));
+            var productsPage = new ProductsPage(Driver);
 
-            Actions actions = new Actions(Driver);
+            productsPage.OpenPage();
 
-            actions.KeyDown(Keys.LeftControl).Click(adminButton).KeyUp(Keys.LeftControl);
+            //productsPage.MotherboardButtonClick();
 
-            IAction multiple = actions.Build();
+            //productsPage.ProductClickOnGridByProductName("Test Motherboard 1");
 
-            multiple.Perform();
+            //productsPage.ProductClickOnGridByRowNumber(2);
 
-            bool isSuccessful = true;
+            //productsPage.SearchByBrand("Asus");
 
-            if (Driver.WindowHandles.Count != 2)
-            {
-                isSuccessful = false;
-            }
-
-            if (isSuccessful)
-            {
-                Driver.SwitchTo().Window(Driver.WindowHandles[1]);
-
-                Driver.FindElement(By.XPath("//a[@href='/admin/products/update']")).Click();
-
-                Driver.FindElement(By.ClassName("search-input")).SendKeys("ram");
-            }
-
-            //Az elsõ találatra kattints rá a grid-en
-            //Driver.FindElement(By.XPath("//div[@class='ag-row-even ag-row-no-focus ag-row ag-row-level-0 ag-row-position-absolute ag-row-first ag-after-created']/div[1]")).Click();
-
-            //Kattints rá a törlés gombra
-            //Driver.FindElement(By.XPath("//button[text()='Delete']")).Click();
-
-            //Válts át a felugró megerõsítõ ablakra és fogadd el
-            //Driver.SwitchTo().Alert().Accept();
+            productsPage.SearchByName("moth");
 
             Assert.Pass();
         }
